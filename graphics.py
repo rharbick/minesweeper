449e01452a1e7894e298b0b174fca5479a1c2490
Riley Harbick <rileyharbick@gmail.com>
2020-12-09 16:52:21 -0500

    Add graphics.py

---
+++ graphics.py
@@ -0,0 +1,12 @@
+import pygame
+
+window = pygame.display.set_mode((500, 500))
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
